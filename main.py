cefc76c44e20ad93a452b71d66355c1efa7f5385
Lukas de Oliveira <lukas_correa_xp@hotmail.com>
2024-06-10 21:03:48 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de Dados")
+
+# Variáveis
+idade = 32
+nome = "Lukas"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1, 2, 3, 4, idade, nova_idade]
+
+# Métodos
+lista_invertida = lista.reverse()
